94e31f341c11dd85fe901a49dea5ee7632097516
m.ghasemi <m.ghasemi@citydi.com>
2023-10-30 16:41:08 +0330

    'init'

---
+++ src/main/java/com/azin/TestingJavawithJUnit5Mockito/TestingJavawithJUnit5MockitoApplication.java
@@ -0,0 +1,13 @@
+package com.azin.TestingJavawithJUnit5Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingJavawithJUnit5MockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingJavawithJUnit5MockitoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/azin/TestingJavawithJUnit5Mockito/service/Calculator.java
@@ -0,0 +1,12 @@
+package com.azin.TestingJavawithJUnit5Mockito.service;
+
+public class Calculator {
+
+    public int sum(int i, int j) {
+        return i + j;
+    }
+
+    public int division(int i, int j) {
+        return i / j;
+    }
+}
